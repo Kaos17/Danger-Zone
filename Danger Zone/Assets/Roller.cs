31c49250033cee4514b7b2bc031c656b36ff35ed
Kyle <kaos17@cableone.net>
2018-01-18 20:48:06 -0800

    Dice!
    This is a single die that can be tossed around. I haven't made it a d4 yet, just wanted to upload as soon as I had the movement working so it didn't somehow get ruined.

---
+++ Danger Zone/Assets/Roller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roller : MonoBehaviour {
+	
+	public float xThrust;
+	public float yThrust;
+	public float zThrust;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			xThrust = Random.Range(-6, 6);
+			yThrust = Random.Range(-6, 6);
+			zThrust = Random.Range(-6, 6);
+			rb.AddForce(xThrust, yThrust, zThrust, ForceMode.Impulse);
+		}
+	}
+}
