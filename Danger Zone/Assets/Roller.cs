1b029da13ecc608ba65b9c0a7f46ed49d18617e5
Kyle <kaos17@cableone.net>
2018-01-19 18:40:05 -0800

    Best Dice
    Can now roll a specificed number of d4 all at once. Still no sensors, since there's no indication of which side is the 1 yet.

--- Danger Zone/Assets/Roller.cs
+++ Danger Zone/Assets/Roller.cs
@@ -13,17 +13,17 @@ public class Roller : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		xThrust = Random.Range(-10, 10);
-		yThrust = Random.Range(-10, 10);
-		zThrust = Random.Range(-10, 10);
-		if(Mathf.Abs(xThrust)<3){xThrust=3;}
+		yThrust = Random.Range(4, 6);
+		zThrust = Random.Range(4, 10);
+		/*if(Mathf.Abs(xThrust)<3){xThrust=3;}
 		if(Mathf.Abs(yThrust)<3){yThrust=3;}
-		if(Mathf.Abs(zThrust)<3){zThrust=3;}
+		if(Mathf.Abs(zThrust)<3){zThrust=3;}*/
 		rb.AddForce(xThrust, yThrust, zThrust, ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space))
+		/*if(Input.GetKeyDown(KeyCode.Space))
 		{
 			xThrust = Random.Range(-10, 10);
 			yThrust = Random.Range(-10, 10);
@@ -32,6 +32,6 @@ public class Roller : MonoBehaviour {
 			if(Mathf.Abs(yThrust)<3){yThrust=3;}
 			if(Mathf.Abs(zThrust)<3){zThrust=3;}
 			rb.AddForce(xThrust, yThrust, zThrust, ForceMode.Impulse);
-		}
+		}*/
 	}
 }
---
+++ Danger Zone/Assets/Tosser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tosser : MonoBehaviour {
+	
+	public int count;
+	public GameObject die;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.Space)){
+			for(int i=0; i<count; i++){
+				Instantiate(die, new Vector3(Random.Range(-7f, 7f), 3.5f, -7f), Quaternion.identity);
+			}
+		}
+	}
+}
