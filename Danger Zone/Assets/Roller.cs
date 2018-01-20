5da9bc57755634f4e0070f6e600529fe41ff37d2
Kyle <kaos17@cableone.net>
2018-01-19 16:53:52 -0800

    better dice

--- Danger Zone/Assets/Roller.cs
+++ Danger Zone/Assets/Roller.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class Roller : MonoBehaviour {
 	
-	public float xThrust;
-	public float yThrust;
-	public float zThrust;
+	float xThrust;
+	float yThrust;
+	float zThrust;
 	public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		xThrust = Random.Range(-10, 10);
+		yThrust = Random.Range(-10, 10);
+		zThrust = Random.Range(-10, 10);
+		if(Mathf.Abs(xThrust)<3){xThrust=3;}
+		if(Mathf.Abs(yThrust)<3){yThrust=3;}
+		if(Mathf.Abs(zThrust)<3){zThrust=3;}
+		rb.AddForce(xThrust, yThrust, zThrust, ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			xThrust = Random.Range(-6, 6);
-			yThrust = Random.Range(-6, 6);
-			zThrust = Random.Range(-6, 6);
+			xThrust = Random.Range(-10, 10);
+			yThrust = Random.Range(-10, 10);
+			zThrust = Random.Range(-10, 10);
+			if(Mathf.Abs(xThrust)<3){xThrust=3;}
+			if(Mathf.Abs(yThrust)<3){yThrust=3;}
+			if(Mathf.Abs(zThrust)<3){zThrust=3;}
 			rb.AddForce(xThrust, yThrust, zThrust, ForceMode.Impulse);
 		}
 	}
