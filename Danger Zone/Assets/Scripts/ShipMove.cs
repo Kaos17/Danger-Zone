2fcce348c2211a275b6dd6ae3fa420af380f456e
Kyle <kaos17@cableone.net>
2018-01-22 14:36:51 -0800

    Movement Works

--- Danger Zone/Assets/Scripts/ShipMove.cs
+++ Danger Zone/Assets/Scripts/ShipMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShipMove : MonoBehaviour {
 	
 	public GameObject ship;
+	public GameObject node;
 
 	// Use this for initialization
 	void Start () {
@@ -13,19 +14,38 @@ public class ShipMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-	
-	void OnMouseOver(){
 		if(Input.GetMouseButtonDown(0)){
-			while(ship.transform.position.x != transform.position.x){
-				if(ship.transform.position.x < transform.position.x){ship.transform.Translate(Vector3.right * Time.deltaTime);}
-				else{ship.transform.Translate(Vector3.left * Time.deltaTime);}
-			}
-			while(ship.transform.position.z != transform.position.z){
-				if(ship.transform.position.z < transform.position.z){ship.transform.Translate(Vector3.forward * Time.deltaTime);}
-				else{ship.transform.Translate(Vector3.back * Time.deltaTime);}
-			}
+			this.StartCoroutine(SmoothMove(node.transform.position, 0.05f));
 		}
 	}
+	
+	IEnumerator SmoothMove(Vector3 target, float delta)
+     {
+         // Will need to perform some of this process and yield until next frames
+         float closeEnough = 0.2f;
+         float distance = (ship.transform.position - target).magnitude;
+ 
+         // GC will trigger unless we define this ahead of time
+         WaitForEndOfFrame wait = new WaitForEndOfFrame();
+ 
+         // Continue until we're there
+         while(distance >= closeEnough)
+         {
+             // Confirm that it's moving
+             Debug.Log("Executing Movement");
+ 
+             // Move a bit then  wait until next  frame
+             ship.transform.position = Vector3.Slerp(ship.transform.position, target, delta);
+             yield return wait;
+ 
+             // Check if we should repeat
+             distance = (ship.transform.position - target).magnitude;
+         }
+ 
+         // Complete the motion to prevent negligible sliding
+         ship.transform.position = target;
+ 
+         // Confirm  it's ended
+         Debug.Log ("Movement Complete");
+     }
 }
