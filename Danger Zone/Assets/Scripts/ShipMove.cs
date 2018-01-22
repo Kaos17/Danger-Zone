852a6b2bdbab955263e2f3468c4459ff5e1c598b
Kyle <kaos17@cableone.net>
2018-01-21 16:44:32 -0800

    Bugged movement

---
+++ Danger Zone/Assets/Scripts/ShipMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour {
+	
+	public GameObject ship;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseOver(){
+		if(Input.GetMouseButtonDown(0)){
+			while(ship.transform.position.x != transform.position.x){
+				if(ship.transform.position.x < transform.position.x){ship.transform.Translate(Vector3.right * Time.deltaTime);}
+				else{ship.transform.Translate(Vector3.left * Time.deltaTime);}
+			}
+			while(ship.transform.position.z != transform.position.z){
+				if(ship.transform.position.z < transform.position.z){ship.transform.Translate(Vector3.forward * Time.deltaTime);}
+				else{ship.transform.Translate(Vector3.back * Time.deltaTime);}
+			}
+		}
+	}
+}
