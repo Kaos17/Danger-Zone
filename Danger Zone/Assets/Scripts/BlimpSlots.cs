a57fc0cc33f30ed4356fc5f98926eb98f086d18c
Kyle <kaos17@cableone.net>
2018-01-28 15:13:49 -0800

    basic augment slots

---
+++ Danger Zone/Assets/Scripts/BlimpSlots.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlimpSlots : MonoBehaviour {
+
+	public GameObject blank;
+	public Vector3 top;
+	public Vector3 bottom;
+	public Vector3 left;
+	public Vector3 right;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (blank, top, Quaternion.identity);
+		Instantiate (blank, bottom, Quaternion.identity);
+		Instantiate (blank, left, Quaternion.identity);
+		Instantiate (blank, right, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
