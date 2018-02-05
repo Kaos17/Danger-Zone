9872e6bbacd9d0316281d2c4a6c47c7bb3e41b04
Kyle <kaos17@cableone.net>
2018-02-04 16:51:45 -0800

    Boom

--- Danger Zone/Assets/Standard Assets/ParticleSystems/Scripts/ParticleSystemMultiplier.cs
+++ Danger Zone/Assets/Standard Assets/ParticleSystems/Scripts/ParticleSystemMultiplier.cs
@@ -1,27 +1,38 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 namespace UnityStandardAssets.Effects
 {
     public class ParticleSystemMultiplier : MonoBehaviour
     {
+		public GameObject ship;
         // a simple script to scale the size, speed and lifetime of a particle system
 
         public float multiplier = 1;
 
 
-        private void Start()
+		private void Update()
         {
-            var systems = GetComponentsInChildren<ParticleSystem>();
-            foreach (ParticleSystem system in systems)
-            {
-				ParticleSystem.MainModule mainModule = system.main;
-				mainModule.startSizeMultiplier *= multiplier;
-                mainModule.startSpeedMultiplier *= multiplier;
-                mainModule.startLifetimeMultiplier *= Mathf.Lerp(multiplier, 1, 0.5f);
-                system.Clear();
-                system.Play();
-            }
+			if (Input.GetKeyDown (KeyCode.E)) {
+				var systems = GetComponentsInChildren<ParticleSystem> ();
+				foreach (ParticleSystem system in systems) {
+					ParticleSystem.MainModule mainModule = system.main;
+					mainModule.startSizeMultiplier *= multiplier;
+					mainModule.startSpeedMultiplier *= multiplier;
+					mainModule.startLifetimeMultiplier *= Mathf.Lerp (multiplier, 1, 0.5f);
+					system.Clear ();
+					system.Play ();
+				}
+				transform.parent = null;
+				Destroy (ship);
+				StartCoroutine (pause());
+				Destroy (this);
+			}
         }
+
+		IEnumerator pause(){
+			yield return new WaitForSeconds (3);
+		}
     }
 }
