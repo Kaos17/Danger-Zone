13cbc2d6954348b6102c14ad0dcb4eca51ea4f84
Kyle <kaos17@cableone.net>
2018-02-14 02:19:49 -0800

    Restart button

---
+++ Danger Zone/Assets/Scripts/Restart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour {
+
+	public Button butt;
+
+	// Use this for initialization
+	void Start () {
+		butt.onClick.AddListener (HandleClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void HandleClick(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+}
