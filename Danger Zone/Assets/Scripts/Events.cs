f20a9918e1792daa33261e44becfe83b6b91d042
Kyle <kaos17@cableone.net>
2018-03-15 23:24:19 -0700

    all the elements are in place

---
+++ Danger Zone/Assets/Scripts/Events.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Events : MonoBehaviour {
+
+	public string[][] eventQueue = new string[10][];
+	public Text description;
+	public Text title;
+	public Text bubble1;
+	public Text bubble2;
+	public Text bubble3;
+	public Button choice1;
+	public Button choice2;
+	public Button choice3;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < eventQueue.Length; x++) {
+			eventQueue [x] = new string[41];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
